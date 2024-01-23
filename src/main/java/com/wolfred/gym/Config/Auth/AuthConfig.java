8495929fe652d3b1a2ffe2fb4eafd2b3d06c39a6
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-01-23 16:01:15 -0500

    add securityFilter, AuthService, TokenProvider and AuthConfig and the env example

---
+++ src/main/java/com/wolfred/gym/Config/Auth/AuthConfig.java
@@ -0,0 +1,48 @@
+package com.wolfred.gym.Config.Auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+  @Autowired
+  SecurityFilter securityFilter;
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/api/signup").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+            //.requestMatchers(HttpMethod.POST, "/api/v1/books").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.GET, "/api/clubs/**").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ src/main/java/com/wolfred/gym/Service/AuthService.java
@@ -0,0 +1,38 @@
+package com.wolfred.gym.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.wolfred.gym.Dto.SignUpDto;
+import com.wolfred.gym.Models.User;
+import com.wolfred.gym.Repository.UserRepository;
+
+
+
+@Service
+public class AuthService implements UserDetailsService {
+
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    var user = repository.findByEmail(username);
+    return user;
+  }
+
+  public UserDetails signUp(SignUpDto data){
+    if (repository.findByEmail(data.email()) != null) {
+        return null;
+    }
+    String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+    User newUser = new User();
+    newUser.setEmail(data.email());
+    newUser.setPassword(encryptedPassword);
+    newUser.setRole(data.role());
+    return repository.save(newUser);
+  }
+}
