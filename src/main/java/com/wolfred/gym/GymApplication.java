32ac3f08dde014cfd3510eaf025ae44efac8c4b8
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-01-23 15:57:57 -0500

    add ModelWrapper to transform to Dto

--- src/main/java/com/wolfred/gym/GymApplication.java
+++ src/main/java/com/wolfred/gym/GymApplication.java
@@ -1,7 +1,9 @@
 package com.wolfred.gym;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GymApplication {
@@ -9,5 +11,10 @@ public class GymApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GymApplication.class, args);
 	}
+
+	@Bean
+    public ModelMapper getModelMapper() { 
+        return new ModelMapper(); 
+    } 
 
 }
