438a1d1026c773574992ad8cfb019f055a413432
Wolfred Montilla <wolfrednicolas@gmail.com>
2024-01-23 19:36:38 -0500

    new class CreateUserAdminRequestDTO

---
+++ src/main/java/com/wolfred/gym/Dto/CreateUserAdminRequestDTO.java
@@ -0,0 +1,25 @@
+package com.wolfred.gym.Dto;
+
+import com.wolfred.gym.Enums.UserRole;
+
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserAdminRequestDTO(
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    String email,
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    String password,
+
+    @NotNull(message = "Role is required")
+    @Enumerated
+    UserRole role
+    
+    ) {
+}
